e2db20c43de0a9fdbb611efa5ab2bd74c91f554c
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-16 14:14:20 -0700

    Get 4 directions out of my pond tile by using transforms

--- test/myOwnTiles/pond.js
+++ test/myOwnTiles/pond.js
@@ -1,9 +1,5 @@
 import { GameCanvas } from '../../src/common/GameCanvas.js';
 
-const paths = {
-  top: new Path2D( 'M -0.5,-0.5 L 0.5,-0.5 L 0.5,-0.3 Q -0.2,-0.2 -0.3,0.5 L -0.5,0.5 Z' ),
-}
-
 const gameCanvas = new GameCanvas();
 gameCanvas.bounds = [ -2, -2, 2, 2 ];
 gameCanvas.backgroundColor = '#123';
@@ -22,70 +18,104 @@ gameCanvas.draw = ( ctx ) => {
   // Top
   ctx.save(); {
     // scaleX, skewY, skewX, scaleY, translateX, translateY
-    ctx.transform( 1, 0.5, -1, 0.5, 0, 0 );
+    const IsometricTransforms = {
+      north: [  1,  0.5, -1,  0.5, 0, 0 ],
+      east:  [ -1,  0.5, -1, -0.5, 0, 0 ],
+      south: [ -1, -0.5,  1, -0.5, 0, 0 ],
+      west:  [  1, -0.5,  1,  0.5, 0, 0 ],
+    };
+
+    const DownTransforms = {
+      north: [ 1, 0, 0, 1,  0.2,  0.2 ],
+      east:  [ 1, 0, 0, 1,  0.2, -0.2 ],
+      south: [ 1, 0, 0, 1, -0.2, -0.2 ],
+      west:  [ 1, 0, 0, 1, -0.2,  0.2 ],
+    };
+
+    const Direction = 'north';
+
+    ctx.transform( ...IsometricTransforms[ Direction ] );
+    const transformDown = DownTransforms[ Direction ];
+
+    // Water
+    ctx.save(); {
+      ctx.transform( ...transformDown );
+
+      ctx.beginPath();
+      ctx.moveTo( 0.5, -0.3 );
+      ctx.quadraticCurveTo( -0.2, -0.2, -0.3, 0.5 );
+      ctx.lineTo( 0.5, 0.5 );
+      ctx.closePath();
+
+      ctx.fillStyle = 'cyan';
+      ctx.fill();
+    }
+    ctx.restore();
+
+    // Downward
+    ctx.save(); {
+      ctx.beginPath();
+      ctx.moveTo( 0.5, -0.3 );
+      ctx.quadraticCurveTo( -0.2, -0.2, -0.3, 0.5 );
+
+      ctx.transform( ...transformDown );
+
+      ctx.lineTo( -0.3, 0.5 );
+      ctx.quadraticCurveTo( -0.2, -0.2, 0.5, -0.3 );
+      ctx.closePath();
+
+      ctx.fillStyle = '#060';
+      ctx.fill();
+    }
+    ctx.restore();
+
+    // Top
+    ctx.beginPath();
+    ctx.moveTo( -0.5, -0.5 );
+    ctx.lineTo( 0.5, -0.5 );
+    ctx.lineTo( 0.5, -0.3 );
+    ctx.quadraticCurveTo( -0.2, -0.2, -0.3, 0.5 );
+    ctx.lineTo( -0.5, 0.5 );
+    ctx.closePath();
 
     ctx.fillStyle = 'green';
-    // ctx.fillRect( -0.5, -0.5, 1, 1 );
-
-    ctx.fill ( paths.top );
-
-
-    ctx.beginPath();
-    ctx.moveTo( 0.5, -0.3 );
-    ctx.quadraticCurveTo( -0.2, -0.2, -0.3, 0.5 );
-
-    ctx.transform( 1, 0, 0, 1, 0.2, 0.2 );  // go down
-
-    ctx.lineTo( -0.3, 0.5 );
-    ctx.quadraticCurveTo( -0.2, -0.2, 0.5, -0.3 );
-    ctx.closePath();
-
-    ctx.fillStyle = '#060';
     ctx.fill();
 
-    ctx.beginPath();
-    ctx.moveTo( 0.5, -0.3 );
-    ctx.quadraticCurveTo( -0.2, -0.2, -0.3, 0.5 );
-    ctx.lineTo( 0.5, 0.5 );
-    ctx.closePath();
-
-    ctx.fillStyle = 'cyan';
-    ctx.fill();
 
     // clear path so next ones don't screw it up
     ctx.beginPath();
   }
   ctx.restore();
 
-  // Left
-  ctx.save(); {
-    // scaleX, skewY, skewX, scaleY, translateX, translateY
-    ctx.transform( 1, 0.5, 0, 1, -0.5, 0.75 );
+  // // Left
+  // ctx.save(); {
+  //   // scaleX, skewY, skewX, scaleY, translateX, translateY
+  //   ctx.transform( 1, 0.5, 0, 1, -0.5, 0.75 );
 
-    ctx.fillStyle = 'tan';
-    ctx.fillRect( -0.5, -0.3, 1, 1 - 0.2 );
+  //   ctx.fillStyle = 'tan';
+  //   ctx.fillRect( -0.5, -0.3, 1, 1 - 0.2 );
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect( -0.5, -0.5, 0.2, 0.2 );
+  //   ctx.fillStyle = 'green';
+  //   ctx.fillRect( -0.5, -0.5, 0.2, 0.2 );
 
-    ctx.fill();
-  }
-  ctx.restore();
+  //   ctx.fill();
+  // }
+  // ctx.restore();
 
-  // Right
-  ctx.save(); {
-    // scaleX, skewY, skewX, scaleY, translateX, translateY
-    ctx.transform( 1, -0.5, 0, 1, 0.5, 0.75 );
+  // // Right
+  // ctx.save(); {
+  //   // scaleX, skewY, skewX, scaleY, translateX, translateY
+  //   ctx.transform( 1, -0.5, 0, 1, 0.5, 0.75 );
 
-    ctx.fillStyle = 'tan';
-    ctx.fillRect( -0.5, -0.3, 1, 1 - 0.2 );
+  //   ctx.fillStyle = 'tan';
+  //   ctx.fillRect( -0.5, -0.3, 1, 1 - 0.2 );
 
-    ctx.fillStyle = 'green';
-    ctx.fillRect( 0.3, -0.5, 0.2, 0.2 );
+  //   ctx.fillStyle = 'green';
+  //   ctx.fillRect( 0.3, -0.5, 0.2, 0.2 );
 
-    // drawDirt( ctx );
-  }
-  ctx.restore();
+  //   // drawDirt( ctx );
+  // }
+  // ctx.restore();
 }
 
 gameCanvas.redraw();
