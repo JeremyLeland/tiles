9ac1df031ae9c3febd62540075e2084b47aa58e2
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-15 20:12:06 -0700

    Output tile name and location to JSON

--- tools/combine_tiles.py
+++ tools/combine_tiles.py
@@ -1,10 +1,11 @@
 from PIL import Image
 from pathlib import Path
+import json
 
 folder = '/home/iggames/Downloads/kenney/kenney_sketch-desert/Tiles/'
 # output_path = '/home/iggames/Downloads/kenney_sketch-desert_combined_ADAPTIVE_optimize9.png'
 # output_path = '/home/iggames/Downloads/kenney_sketch-desert_combined_optimize9.png'
-output_path = '/home/iggames/Downloads/kenney_sketch-desert_combined.png'
+output_path = '/home/iggames/Downloads/kenney_sketch-desert_combined'
 
 # files = [ f for f in os.listdir( folder ) if f.endswith( '.png' ) ]
 files = list( Path( folder ).glob( '*png' ) )
@@ -22,11 +23,20 @@ cols = int( len( imgs ) / 4 )
 # Create new blank image
 combined = Image.new( 'RGBA', ( cols * max_width, 4 * max_height ) )
 
+combined_json = {}
+
 # Paste each image
 x_offset = 0
 y_offset = 0
 for img in imgs:
-  print( img, 'colors: ', len( img.getcolors(maxcolors=10_000_000) ) )
+  name = Path( img.filename ).stem
+  sx = x_offset * img.width
+  sy = y_offset * img.height
+  sw = img.width
+  sh = img.height
+
+  combined_json[ name ] = [ sx, sy, sw, sh ]
+
   combined.paste( img, ( x_offset * img.width, y_offset * img.height ) )
   y_offset += 1
   if y_offset == 4:
@@ -36,13 +46,12 @@ for img in imgs:
 
 # combined = combined.convert("P", palette=Image.ADAPTIVE)  # lossy
 # combined.save( output_path, optimize=True, compress_level=9 ) # slower
-combined.save( output_path )
+combined.save( output_path + '.png' )
 
-print( 'Combined colors: ', len( combined.getcolors(maxcolors=10_000_000) ) )
+# print( 'Combined colors: ', len( combined.getcolors(maxcolors=10_000_000) ) )
 
 # If colors <= 256, can use this
 # palette_img = combined.quantize(colors=256, method=Image.MAXCOVERAGE)
 
-# combined = combined.convert("P", palette=Image.ADAPTIVE)  # lossy
-# combined.save( output_path, optimize=True, compress_level=9 ) # slower
-# combined.save( output_path )
+with open( output_path + '.json', 'w' ) as fp:
+  json.dump( combined_json, fp )
