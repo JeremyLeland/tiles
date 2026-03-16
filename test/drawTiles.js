0b735c9e7a91b66483bb709e581bbd51bdddba8b
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-15 21:58:37 -0700

    Draw multiple layers of tiles (so we can have height)

--- test/drawTiles.js
+++ test/drawTiles.js
@@ -1,32 +1,39 @@
 import { GameCanvas } from '../src/common/GameCanvas.js';
 
-const tilePath = new Path2D( `
-  M -1,0 L 0,-0.5 L 1,0 L 0,0.5 Z
-  M -1,0 L -1,1 L 0,1.5 L 0,0.5 Z
-  M  1,0 L  1,1 L 0,1.5 L 0,0.5 Z
-` );
+const paths = {
+  top: new Path2D( 'M -1,0 L 0,-0.5 L 1,0 L 0,0.5 Z' ),
+  left: new Path2D( 'M -1,0 L -1,1 L 0,1.5 L 0,0.5 Z' ),
+  right: new Path2D( 'M  1,0 L  1,1 L 0,1.5 L 0,0.5 Z' ),
+};
 
-const colors = [ 'green', 'blue', 'brown' ];
+const colors = [ null, 'green', 'blue', 'brown' ];
 
 const rows = 3, cols = 4;
 const map = [
-  0, 1, 0, 0,
-  0, 1, 1, 2,
-  0, 1, 0, 0,
+  1, 2, 1, 1,
+  1, 2, 2, 3,
+  1, 2, 1, 1,
+
+  1, 0, 0, 0,
+  1, 0, 0, 0,
+  0, 0, 0, 0,
 ];
 
-function drawTile( ctx, color, col, row ) {
-  ctx.fillStyle = color;
+function drawTile( ctx, color, col, row, height ) {
 
   const w = 1;
   const h = w / 2;
 
   const x = w * ( col - row );
-  const y = h * ( row + col );
+  const y = h * ( row + col - height * 2 );
 
   ctx.save();
   ctx.translate( x, y );
-  ctx.fill( tilePath );
+  ctx.fillStyle = color;
+  ctx.fill( paths.top );
+  ctx.fillStyle = 'brown';
+  ctx.fill( paths.left );
+  ctx.fill( paths.right );
   ctx.restore();
 }
 
@@ -46,9 +53,15 @@ gameCanvas.draw = ( ctx ) => {
     ctx.fillRect( col, bounds[ 1 ], 0.01, bounds[ 3 ] - bounds[ 1 ] );
   }
 
-  for ( let row = 0; row < rows; row ++ ) {
-    for ( let col = 0; col < cols; col ++ ) {
-      drawTile( ctx, colors[ map[ col + row * cols ] ], col, row );
+  for ( let height = 0; height < 2; height ++ ) {
+    for ( let row = 0; row < rows; row ++ ) {
+      for ( let col = 0; col < cols; col ++ ) {
+        const colorIndex = map[ col + row * cols + height * cols * rows ];
+
+        if ( colorIndex > 0 ) {
+          drawTile( ctx, colors[ colorIndex ], col, row, height );
+        }
+      }
     }
   }
 }
