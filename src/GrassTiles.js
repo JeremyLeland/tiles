62f5cd2b01e111e3dd3873c78d9686cd870f811b
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-24 20:57:10 -0700

    Break simpleWebGL.js into several common sources. Re-arrange so that meshes are created as needed (since that's when we have gl object)

--- src/GrassTiles.js
+++ src/GrassTiles.js
@@ -1,12 +1,8 @@
-import { GLGameCanvas } from '../src/common/GLGameCanvas.js';
 import * as Angle from '../src/common/Angle.js';
+import * as MeshCommon from '../src/common/MeshCommon.js';
 import * as ShaderCommon from '../src/common/ShaderCommon.js';
-import { mat4, vec3 } from '../lib/gl-matrix.js';
-import { OrbitCamera } from '../src/common/OrbitCamera.js';
 
-const glGameCanvas = new GLGameCanvas();
-
-const Viewport = [ -4, -4, 4, 4 ];
+import { mat4 } from '../lib/gl-matrix.js';
 
 const GrassHeight = 0.2;
 const GrassEdge = 0.5;
@@ -683,305 +679,105 @@ const waterGeo = {
   ],
 };
 
+// const yRot = ( angle ) => [ 0, Math.sin( angle / 2 ), 0, Math.cos( angle / 2 ) ];
+// const ROT_0 = yRot( 0 );
+// const ROT_90 = yRot( -Math.PI / 2 );
+// const ROT_180 = yRot( Math.PI );
+// const ROT_270 = yRot( Math.PI / 2 );
 
-const shader = ShaderCommon.getShader( glGameCanvas.gl, ShaderCommon.BasicLighting );
-const grassMeshes = {
-  smallCurve:     createMesh( glGameCanvas.gl, smallCurveGeometry, shader ),
-  edge:           createMesh( glGameCanvas.gl, edgeGeometry, shader ),
-  twoSmallCurve:  createMesh( glGameCanvas.gl, twoSmallCurveGeometry, shader ),
-  bigCurve:       createMesh( glGameCanvas.gl, bigCurveGeometry, shader ),
-  full:           createMesh( glGameCanvas.gl, fullGeometry, shader ),
-};
-const waterMesh = createMesh( glGameCanvas.gl, waterGeo, shader );
+const ROT_0 = 0;
+const ROT_90 = -Math.PI / 2;
+const ROT_180 = Math.PI;
+const ROT_270 = Math.PI / 2;
 
-const yRot = ( angle ) => [ 0, Math.sin( angle / 2 ), 0, Math.cos( angle / 2 ) ];
-const ROT_0 = yRot( 0 );
-const ROT_90 = yRot( -Math.PI / 2 );
-const ROT_180 = yRot( Math.PI );
-const ROT_270 = yRot( Math.PI / 2 );
-
-const Tiles = [
-
+const GrassInfo = [
   // NW 0, NE 0, SW 0, SE 0
   null,
 
   // NW 0, NE 0, SW 0, SE 1
-  { mesh: grassMeshes.smallCurve, quat: ROT_180 },
+  { geo: smallCurveGeometry, rot: ROT_180 },
 
   // NW 0, NE 0, SW 1, SE 0
-  { mesh: grassMeshes.smallCurve, quat: ROT_270 },
+  { geo: smallCurveGeometry, rot: ROT_270 },
 
   // NW 0, NE 0, SW 1, SE 1
-  { mesh: grassMeshes.edge, quat: ROT_270 },
+  { geo: edgeGeometry, rot: ROT_270 },
 
   // NW 0, NE 1, SW 0, SE 0
-  { mesh: grassMeshes.smallCurve, quat: ROT_90 },
+  { geo: smallCurveGeometry, rot: ROT_90 },
 
   // NW 0, NE 1, SW 0, SE 1
-  { mesh: grassMeshes.edge, quat: ROT_180 },
+  { geo: edgeGeometry, rot: ROT_180 },
 
   // NW 0, NE 1, SW 1, SE 0
-  { mesh: grassMeshes.twoSmallCurve, quat: ROT_90 },
+  { geo: twoSmallCurveGeometry, rot: ROT_90 },
 
   // NW 0, NE 1, SW 1, SE 1
-  { mesh: grassMeshes.bigCurve, quat: ROT_180 },
+  { geo: bigCurveGeometry, rot: ROT_180 },
 
   // NW 1, NE 0, SW 0, SE 0
-  { mesh: grassMeshes.smallCurve, quat: ROT_0 },
+  { geo: smallCurveGeometry, rot: ROT_0 },
 
   // NW 1, NE 0, SW 0, SE 1
-  { mesh: grassMeshes.twoSmallCurve, quat: ROT_0 },
+  { geo: twoSmallCurveGeometry, rot: ROT_0 },
 
   // NW 1, NE 0, SW 1, SE 0
-  { mesh: grassMeshes.edge, quat: ROT_0 },
+  { geo: edgeGeometry, rot: ROT_0 },
 
   // NW 1, NE 0, SW 1, SE 1
-  { mesh: grassMeshes.bigCurve, quat: ROT_270 },
+  { geo: bigCurveGeometry, rot: ROT_270 },
 
   // NW 1, NE 1, SW 0, SE 0
-  { mesh: grassMeshes.edge, quat: ROT_90 },
+  { geo: edgeGeometry, rot: ROT_90 },
 
   // NW 1, NE 1, SW 0, SE 1
-  { mesh: grassMeshes.bigCurve, quat: ROT_90 },
+  { geo: bigCurveGeometry, rot: ROT_90 },
 
   // NW 1, NE 1, SW 1, SE 0
-  { mesh: grassMeshes.bigCurve, quat: ROT_0 },
+  { geo: bigCurveGeometry, rot: ROT_0 },
 
   // NW 1, NE 1, SW 1, SE 1
-  { mesh: grassMeshes.full, quat: ROT_0 },
+  { geo: fullGeometry, rot: ROT_0 },
 ];
 
-const cols = 5, rows = 6;
-const grassLayer = [
-  1, 1, 1, 1, 0,
-  1, 1, 0, 0, 0,
-  1, 0, 1, 1, 0,
-  1, 0, 1, 1, 0,
-  0, 0, 0, 1, 1,
-  1, 0, 1, 0, 1,
-];
+const ourModelMatrix = mat4.create();
 
+let grassShader, waterShader, waterMesh;
 
-const gridGeo = {
-  positions: [],
-}
+export function drawGrass( gl, type, modelMatrix, viewProjMatrix ) {
 
-for ( let col = 0; col <= cols; col ++ ) {
-  gridGeo.positions.push( col, 0.5001, 0 );
-  gridGeo.positions.push( col, 0.5001, rows );
-}
+  grassShader ??= ShaderCommon.getShader( gl, ShaderCommon.BasicLighting );
+  waterShader ??= grassShader;    // for now
 
-for ( let row = 0; row <= rows; row ++ ) {
-  gridGeo.positions.push( 0, 0.5001, row );
-  gridGeo.positions.push( cols, 0.5001, row );
-}
+  const grassInfo = GrassInfo[ type ];
 
-const colorShader = ShaderCommon.getShader( glGameCanvas.gl, ShaderCommon.SolidColor );
-const gridMesh = createLineMesh( glGameCanvas.gl, gridGeo, colorShader );
+  mat4.rotateY( ourModelMatrix, modelMatrix, grassInfo?.rot ?? 0 );
 
-// const cameraViewMatrix = mat4.lookAt( [], [ 10, 20, 10 ], [ 0, 0, 0 ], [ 0, 1, 0 ] );
-// const cameraPos = [ -2, 0, -2 ];
+  const mvp = mat4.mul( [], viewProjMatrix, ourModelMatrix );
 
-const camera = new OrbitCamera();
-
-glGameCanvas.draw = ( gl ) => {
-  const modelMatrix = mat4.create();
-  const viewMatrix = camera.getViewMatrix();  //mat4.translate( [], cameraViewMatrix, cameraPos );
-
-
-  // const minWidth = Viewport[ 2 ] - Viewport[ 0 ];
-  // const minHeight = Viewport[ 3 ] - Viewport[ 1 ];
-
-  // const xScale = gl.canvas.clientWidth / minWidth;
-  // const yScale = gl.canvas.clientHeight / minHeight;
-
-  // const scale = Math.min( xScale, yScale );
-  // const offsetX = ( minWidth - gl.canvas.clientWidth / scale ) / 2;
-  // const offsetY = ( minHeight - gl.canvas.clientHeight / scale ) / 2;
-
-  // const projMatrix = mat4.ortho(
-  //   [],
-  //   Viewport[ 0 ] + offsetX,
-  //   Viewport[ 2 ] - offsetX,
-  //   Viewport[ 1 ] + offsetY,
-  //   Viewport[ 3 ] - offsetY,
-  //   0,
-  //   100
-  // );
-
-  const projMatrix = mat4.perspective( [], Math.PI / 4, gl.canvas.clientWidth / gl.canvas.clientHeight, 0.1, 100 );
-
-  const mvp = mat4.mul( [], viewMatrix, modelMatrix );
-  mat4.mul( mvp, projMatrix, mvp );
-
-  const normalMatrix = mat4.invert( [], modelMatrix );
+  const normalMatrix = mat4.invert( [], ourModelMatrix );
   mat4.transpose( normalMatrix, normalMatrix );
 
-  gl.useProgram( gridMesh.shader.program );
-  gl.uniformMatrix4fv( gridMesh.shader.uniformLocations.mvp, false, mvp );
-  gl.uniformMatrix4fv( gridMesh.shader.uniformLocations.normalMatrix, false, normalMatrix );
+  if ( grassInfo ) {
+    gl.useProgram( grassShader.program );
+    gl.uniformMatrix4fv( grassShader.uniformLocations.mvp, false, mvp );
+    gl.uniformMatrix4fv( grassShader.uniformLocations.normalMatrix, false, normalMatrix );
 
-  gl.uniform3fv( gridMesh.shader.uniformLocations.color, [ 1, 1, 1 ] );
+    gl.uniform3fv( grassShader.uniformLocations.color, [ 0, 1, 0 ] );
 
-  gl.bindVertexArray( gridMesh.vao );
-  gl.drawArrays( gl.LINES, 0, gridMesh.geometry.positions.length / 3 );
-
-  for ( let row = 0; row <= rows; row ++ ) {
-    for ( let col = 0; col <= cols; col ++ ) {
-      const wc = col == 0 ? 0 : col - 1;
-      const ec = col == ( cols ) ? ( cols - 1 ) : ( col );
-      const nr = row == 0 ? 0 : row - 1;
-      const sr = row == ( rows ) ? ( rows - 1 ) : ( row );
-
-      const nw = grassLayer[ wc + nr * cols ];
-      const ne = grassLayer[ ec + nr * cols ];
-      const sw = grassLayer[ wc + sr * cols ];
-      const se = grassLayer[ ec + sr * cols ];
-
-      const grassInfo = Tiles[ ( nw << 3 ) + ( ne << 2 ) + ( sw << 1 ) + se ];
-
-      mat4.fromRotationTranslation(
-        modelMatrix,
-        grassInfo?.quat ?? [ 0, 0, 0, 1 ],
-        [ col, 0, row ],
-      );
-
-      const mvp = mat4.mul( [], viewMatrix, modelMatrix );
-      mat4.mul( mvp, projMatrix, mvp );
-
-      const normalMatrix = mat4.invert( [], modelMatrix );
-      mat4.transpose( normalMatrix, normalMatrix );
-
-      if ( grassInfo ) {
-        gl.useProgram( grassInfo.mesh.shader.program );
-        gl.uniformMatrix4fv( grassInfo.mesh.shader.uniformLocations.mvp, false, mvp );
-        gl.uniformMatrix4fv( grassInfo.mesh.shader.uniformLocations.normalMatrix, false, normalMatrix );
-
-        gl.uniform3fv( grassInfo.mesh.shader.uniformLocations.color, [ 0, 1, 0 ] );
-
-        gl.bindVertexArray( grassInfo.mesh.vao );
-        gl.drawElements( gl.TRIANGLES, grassInfo.mesh.geometry.indices.length, gl.UNSIGNED_SHORT, 0 );
-      }
-
-      gl.useProgram( waterMesh.shader.program );
-      gl.uniformMatrix4fv( waterMesh.shader.uniformLocations.mvp, false, mvp );
-      gl.uniformMatrix4fv( waterMesh.shader.uniformLocations.normalMatrix, false, normalMatrix );
-
-      gl.uniform3fv( waterMesh.shader.uniformLocations.color, [ 0, 0, 1 ] );
-
-      gl.bindVertexArray( waterMesh.vao );
-      gl.drawElements( gl.TRIANGLES, waterMesh.geometry.indices.length, gl.UNSIGNED_SHORT, 0 );
-    }
-  }
-}
-
-const TranslateSpeed = 0.05;
-const Translate = {
-  Up:     () => camera.pan( 0, -TranslateSpeed ),
-  Down:   () => camera.pan( 0,  TranslateSpeed ),
-  Left:   () => camera.pan( -TranslateSpeed, 0 ),
-  Right:  () => camera.pan(  TranslateSpeed, 0 ),
-};
-
-const TranslateKeys = {
-  'w': Translate.Up,
-  'a': Translate.Left,
-  's': Translate.Down,
-  'd': Translate.Right,
-};
-
-document.addEventListener( 'keydown', e => {
-  TranslateKeys[ e.key ]?.();
-
-  glGameCanvas.redraw();
-} );
-
-const X_TURN_SENSITIVITY = 100;
-const Y_TURN_SENSITIVITY = -100;
-const X_MOVE_SENSITIVITY = -50;
-const Y_MOVE_SENSITIVITY = -50;
-
-glGameCanvas.canvas.addEventListener( 'pointermove', e => {
-  // Rotate around origin with left mouse button
-  if ( e.buttons == 1 ) {
-    const dPhi   = e.movementX / X_TURN_SENSITIVITY;
-    const dTheta = e.movementY / Y_TURN_SENSITIVITY;
-
-    camera.rotate( dPhi, dTheta );
-
-    glGameCanvas.redraw();
+    grassInfo.mesh ??= MeshCommon.createMesh( gl, grassInfo.geo, grassShader );
+    gl.bindVertexArray( grassInfo.mesh.vao );
+    gl.drawElements( gl.TRIANGLES, grassInfo.mesh.geometry.indices.length, gl.UNSIGNED_SHORT, 0 );
   }
 
-  // Pan with right mouse button
-  else if ( e.buttons == 2 ) {
-    const dx = e.movementX / X_MOVE_SENSITIVITY;
-    const dy = e.movementY / Y_MOVE_SENSITIVITY;
+  gl.useProgram( waterShader.program );
+  gl.uniformMatrix4fv( waterShader.uniformLocations.mvp, false, mvp );
+  gl.uniformMatrix4fv( waterShader.uniformLocations.normalMatrix, false, normalMatrix );
 
-    camera.pan( dx, dy );
+  gl.uniform3fv( waterShader.uniformLocations.color, [ 0, 0, 1 ] );
 
-    glGameCanvas.redraw();
-  }
-} );
+  waterMesh ??= MeshCommon.createMesh( gl, waterGeo, waterShader );
+  gl.bindVertexArray( waterMesh.vao );
+  gl.drawElements( gl.TRIANGLES, waterMesh.geometry.indices.length, gl.UNSIGNED_SHORT, 0 );
 
-const ZOOM_SENSIVITY = -200;
-
-glGameCanvas.canvas.addEventListener( 'wheel', e => {
-  camera.zoom( e.wheelDelta / ZOOM_SENSIVITY );
-
-  glGameCanvas.redraw();
-} );
-
-
-function createMesh( gl, geometry, shader ) {
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray( vao );
-
-  gl.bindBuffer( gl.ARRAY_BUFFER, createArrayBuffer( gl, geometry.positions ) );
-  gl.vertexAttribPointer( shader.attribLocations.position, 3, gl.FLOAT, false, 0, 0 );
-  gl.enableVertexAttribArray( shader.attribLocations.position );
-
-  gl.bindBuffer( gl.ARRAY_BUFFER, createArrayBuffer( gl, geometry.normals ) );
-  gl.vertexAttribPointer( shader.attribLocations.normal, 3, gl.FLOAT, false, 0, 0 );
-  gl.enableVertexAttribArray( shader.attribLocations.normal );
-
-  gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, createIndexBuffer( gl, geometry.indices ) );
-
-  gl.bindVertexArray( null );
-
-  return {
-    vao: vao,
-    geometry: geometry,
-    shader: shader,
-  };
-}
-
-function createLineMesh( gl, geometry, shader ) {
-  const vao = gl.createVertexArray();
-  gl.bindVertexArray( vao );
-
-  gl.bindBuffer( gl.ARRAY_BUFFER, createArrayBuffer( gl, geometry.positions ) );
-  gl.vertexAttribPointer( shader.attribLocations.position, 3, gl.FLOAT, false, 0, 0 );
-  gl.enableVertexAttribArray( shader.attribLocations.position );
-
-  gl.bindVertexArray( null );
-
-  return {
-    vao: vao,
-    geometry: geometry,
-    shader: shader,
-  };
-}
-
-function createArrayBuffer( gl, array ) {
-  const arrayBuffer = gl.createBuffer();
-  gl.bindBuffer( gl.ARRAY_BUFFER, arrayBuffer );
-  gl.bufferData( gl.ARRAY_BUFFER, new Float32Array( array ), gl.STATIC_DRAW );
-  return arrayBuffer;
-}
-
-function createIndexBuffer( gl, indices ) {
-  const indexBuffer = gl.createBuffer();
-  gl.bindBuffer( gl.ELEMENT_ARRAY_BUFFER, indexBuffer );
-  gl.bufferData( gl.ELEMENT_ARRAY_BUFFER, new Uint16Array( indices ), gl.STATIC_DRAW );
-  return indexBuffer;
 }
