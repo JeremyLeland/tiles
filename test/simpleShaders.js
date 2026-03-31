8fd56b8ee059eac29d30b2521d230cb976ba070c
Jeremy Leland <jeremy.leland@gmail.com>
2026-03-31 12:26:08 -0700

    Making something grassy with triangles

--- test/simpleShaders.js
+++ test/simpleShaders.js
@@ -27,25 +27,25 @@ const colorShader = ShaderCommon.getShader( glGameCanvas.gl, ShaderCommon.SolidC
 const gridMesh = MeshCommon.createLineMesh( glGameCanvas.gl, gridGeo, colorShader );
 
 const camera = new OrbitCamera( {
-  center: [ 0.5, 0.5, 0 ],
-  distance: 2,
-  phi: Math.PI / 2,
+  center: [ 2, 0, 2 ],
+  distance: 5,
+  phi: Math.PI / 4,
   theta: Math.PI / 3,
 } );
 
 
 const geo = {
   positions: [
-    1, 1, 0,
-    1, 0, 0,
-    0, 1, 0,
+    4, 0, 4,
+    4, 0, 0,
+    0, 0, 4,
     0, 0, 0,
   ],
   normals: [
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
-    0, 0, 1,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
+    0, 1, 0,
   ],
   indices: [
     0, 2, 1,
@@ -80,9 +80,22 @@ const shaderInfo = {
 
     out vec4 outColor;
 
+    float hash(vec2 p) {
+      return fract( sin( dot( p, vec2( 127.1, 311.7 ) ) ) * 43758.5453123 );
+    }
+
+    float ign(vec2 p) {
+      return fract(52.9829189 * fract(dot(p, vec2(0.06711056, 0.00583715))));
+    }
+
+    float ign2(vec2 p) {
+      p += vec2(0.75487765, 0.56984026); // arbitrary offset
+      return fract(52.9829189 * fract(dot(p, vec2(0.06711056, 0.00583715))));
+    }
+
     void main() {
 
-      const float NumSquares = 20.0;
+      const vec3 NumSquares = vec3( 20.0, 1.0, 10.0 );
 
       vec3 integer;
       vec3 floating = modf( mod( v_pos * NumSquares, 2.0 ), integer );
@@ -92,10 +105,15 @@ const shaderInfo = {
 
       float integer3;
 
-      vec3 color1 = vec3( 0, mod( 2.8 * integer2.x + 1.3 * integer2.y, 1.5 ), 0 );
-      vec3 color2 = vec3( 0, mod( 3.1 * integer2.x + 2.4 * integer2.y, 1.5 ), 0 );
+      vec2 cell = integer2.xz;
 
-      if ( floating.x < floating.y ) {
+      float g1 = mix( 0.3, 0.6, hash( cell ) );
+      float g2 = mix( 0.3, 0.6, hash( cell + 17.0 ) ); // offset for variation
+
+      vec3 color1 = vec3( 0.0, g1, 0.0 );
+      vec3 color2 = vec3( 0.0, g2, 0.0 );
+
+      if ( floating.x < floating.z ) {
         outColor = vec4( color1, 1.0 );
       }
       else {
