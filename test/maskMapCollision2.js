5156c722e8470f95eea8e17f54d375762b2a472f
Jeremy Leland <jeremy.leland@gmail.com>
2026-08-12 13:39:18 -0700

    Shoot bullets at walls. Right now they vanish on collision. Next, have them take out part of wall.

---
+++ test/maskMapCollision2.js
@@ -0,0 +1,176 @@
+import { GameCanvas } from '../src/common/GameCanvas.js';
+import { vec2 } from '../lib/gl-matrix.js';
+import * as Util from '../src/common/Util.js';
+
+const Terrain = {
+  Empty: 0,
+  Dirt: 1,
+  Rock: 2,
+};
+
+const cols = 320, rows = 240;
+const map = Array( cols * rows ).fill( Terrain.Dirt );
+
+let playerX = 100, playerY = 100;
+let mouseX, mouseY, cursorRadius = 10;
+
+let bullets = [];
+
+const maskImage = new OffscreenCanvas( cols, rows );
+const maskCtx = maskImage.getContext( '2d' );
+const maskImageData = maskCtx.getImageData( 0, 0, cols, rows );
+const maskData = maskImageData.data;
+
+for ( let index = 0; index < cols * rows; index ++ ) {
+  const maskIndex = 4 * index;
+  maskData[ maskIndex ] = 255;
+  maskData[ maskIndex + 1 ] = 255;
+  maskData[ maskIndex + 2 ] = 255;
+  maskData[ maskIndex + 3 ] = map[ index ] === Terrain.Empty ? 0 : 255;
+}
+
+maskCtx.putImageData( maskImageData, 0, 0 );
+
+function setTerrain( col, row, value ) {
+  if ( 0 <= col && col < cols && 0 <= row && row < rows ) {
+    const mapIndex = col + row * cols;
+    map[ mapIndex ] = value;
+    maskData[ 4 * mapIndex + 3 ] = value;
+  }
+}
+
+function setTerrainCircle( x, y, radius, value ) {
+  for ( let row = y - radius; row < y + radius; row ++ ) {
+    for ( let col = x - radius; col < x + radius; col ++ ) {
+      if ( Math.hypot( col - x, row - y ) < radius ) {
+        setTerrain( col, row, value );
+      }
+    }
+  }
+}
+
+setTerrainCircle( 100, 100, 80, Terrain.Empty );
+setTerrainCircle( 200, 200, 60, Terrain.Empty );
+setTerrainCircle( 250, 50, 60, Terrain.Empty );
+setTerrainCircle( 170, 70, 30, Terrain.Empty );
+
+maskCtx.putImageData( maskImageData, 0, 0 );
+
+
+const foregroundImage = new OffscreenCanvas( cols, rows );
+const foregroundCtx = foregroundImage.getContext( '2d' );
+
+const gameCanvas = new GameCanvas();
+gameCanvas.setBounds( 0, 0, cols, rows );
+
+gameCanvas.update = ( dt ) => {
+  bullets.forEach( bullet => {
+    const hit = getMapHit( map, bullet, dt );
+    if ( hit ) {
+      bullet.health = 0;
+    }
+    else {
+      vec2.scaleAndAdd( bullet.pos, bullet.pos, bullet.vel, dt );
+    }
+  } );
+
+  bullets = bullets.filter( b => b.health > 0 );
+}
+
+gameCanvas.draw = ( ctx ) => {
+  foregroundCtx.clearRect( 0, 0, cols, rows );
+  foregroundCtx.globalCompositeOperation = 'source-over';
+  foregroundCtx.drawImage( maskImage, 0, 0 );
+  foregroundCtx.globalCompositeOperation = 'source-in';
+  foregroundCtx.fillStyle = 'rgb(200, 100, 20)';
+  foregroundCtx.fillRect( 0, 0, cols, rows );
+
+  ctx.imageSmoothingEnabled = false;
+
+  ctx.fillStyle = '#321';
+  ctx.fillRect( 0, 0, cols, rows );
+
+  ctx.drawImage( foregroundImage, 0, 0 );
+
+  //
+  // Cursor
+  //
+
+  // ctx.lineWidth = 0.02;
+  ctx.fillStyle = 'green';
+
+  ctx.beginPath();
+  ctx.arc( playerX, playerY, cursorRadius, 0, Math.PI * 2 );
+  ctx.fill();
+
+  ctx.strokeStyle = 'red';
+
+  Util.drawLine( ctx, [ playerX, playerY ], [ mouseX, mouseY ] );
+
+  ctx.fillStyle = 'white';
+  bullets.forEach( bullet => {
+    Util.drawPoint( ctx, bullet.pos, bullet.radius );
+  } );
+}
+
+function getMapHit( map, entity, dt ) {
+  const lineLen = vec2.length( entity.vel ) * dt;
+  const step = vec2.normalize( [], entity.vel );
+
+  const lineAngle = Math.atan2( step[ 1 ], step[ 0 ] );
+
+  const testCenter = vec2.clone( entity.pos );
+
+  const numChecks = entity.radius * 2;      // radius * 2 is all of the points; fewer checks will space these out
+
+  for ( let i = 0; i <= lineLen; i ++ ) {
+    for ( let j = 0; j <= numChecks; j ++ ) {
+      for ( const dir of [ -1, 1 ] ) {
+        const testAngle = lineAngle + dir * ( j / numChecks ) * Math.PI / 2;
+        const x = testCenter[ 0 ] + Math.cos( testAngle ) * entity.radius;
+        const y = testCenter[ 1 ] + Math.sin( testAngle ) * entity.radius;
+
+        const index = Math.floor( x ) + Math.floor( y ) * cols;
+
+        if ( map[ index ] === Terrain.Dirt ) {
+          return testCenter;
+        }
+      }
+    }
+
+    vec2.add( testCenter, testCenter, step );
+  }
+}
+
+function pointerInput( m ) {
+  mouseX = Math.floor( m.x );
+  mouseY = Math.floor( m.y );
+
+  if ( m.buttons === 1 ) {
+    const lineAngle = Math.atan2( mouseY - playerY, mouseX - playerX );
+    const bulletSpeed = 0.1;
+
+    bullets.push( {
+      pos: [ playerX, playerY ],
+      vel: [ Math.cos( lineAngle ) * bulletSpeed, Math.sin( lineAngle ) * bulletSpeed ],
+      radius: 2,
+      health: 1,
+    } );
+  }
+  else if ( m.buttons === 2 ) {
+    playerX = mouseX;
+    playerY = mouseY;
+  }
+
+  // gameCanvas.redraw();
+}
+
+gameCanvas.pointerDown = pointerInput;
+gameCanvas.pointerMove = pointerInput;
+
+gameCanvas.wheelInput = ( m ) => {
+  cursorRadius = Math.max( 1, Math.min( 50, cursorRadius + Math.sign( m.wheel ) ) );
+  // gameCanvas.redraw();
+}
+
+gameCanvas.start();
